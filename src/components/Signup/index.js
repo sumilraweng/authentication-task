6df445a7b6db8d980bb66f3a669f02373bf9bab6
sumilraweng <sumil.jain@raweng.com>
2021-01-27 17:51:52 +0530

    Added the signup feature and submit functionality in both login and signup

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -7,16 +7,17 @@ import { useState } from "react";
 
 const useStyle = makeStyles((theme) => ({
   signConatiner: {
-    marginTop: theme.spacing(20),
+    marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
   },
 }));
 
-export default function Login() {
+export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const emailInputField = (event) => {
     setEmail(event.target.value);
@@ -26,37 +27,48 @@ export default function Login() {
     setPassword(event.target.value);
   };
 
-  const onSubmitForm = (event) => {
-    event.preventDefault();
-
-    console.log(email, password);
+  const confirmPasswordInputField = (event) => {
+    setConfirmPassword(event.target.value);
+    if (password !== event.target.value) {
+      console.log("password dont match");
+    } else {
+      console.log("password match");
+    }
   };
 
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      alert("password not match");
+    }
+    console.log(email, password, confirmPassword);
+  };
   const classes = useStyle();
   return (
     <Container component="main" maxWidth="xs">
       <div className={classes.signConatiner}>
-        <Typography variant="h5">Sign in</Typography>
+        <Typography variant="h5">Sign up</Typography>
+
         <form noValidate onSubmit={onSubmitForm}>
           <TextField
+            type="email"
             variant="outlined"
             margin="normal"
             id="eamil"
             label="Email Address"
             name="email"
-            type="email"
             fullWidth
             autoFocus
             autoComplete="eamil"
-            required
             value={email}
             onChange={emailInputField}
+            required
           />
 
           <TextField
-            type="password"
             variant="outlined"
             margin="normal"
+            type="password"
             id="password"
             label="Password"
             name="password"
@@ -65,8 +77,22 @@ export default function Login() {
             onChange={passwordInputField}
             required
           />
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            type="password"
+            id="confirm-password"
+            label="Confirm-Password"
+            name="confirm-password"
+            value={confirmPassword}
+            onChange={confirmPasswordInputField}
+            fullWidth
+            required
+          />
+
           <Button type="submit" fullWidth variant="contained" color="primary">
-            Log In{" "}
+            Signup{" "}
           </Button>
         </form>
       </div>
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
 
 export default function Router(props) {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={LoginPage}></Route>
+        <Route path="/" exact component={LoginPage}></Route>
+        <Route path="/signup" component={SignupPage}></Route>
       </Switch>
     </BrowserRouter>
   );
